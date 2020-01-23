737476a297512cf4f64ded71fcc623a1424a8973
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 21:58:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   if name_hash.size > 0 
+    values = []
+    name_hash.collect do |key, value|
+     values << value
+    end 
+    min_value = values[0]
+    values.each do |value|
+      min_value = value if value < min_value 
+    end
+    name_hash.each do |key, value|
+     return key if value == min_value
+    end 
+  end
 end
